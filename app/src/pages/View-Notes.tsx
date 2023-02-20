273992240446389d0d4b64f85c73ba28f4f778b8
al-moy729 <106407272+al-moy729@users.noreply.github.com>
2023-02-20 16:48:34 -0500

    View Note and Components

--- app/src/pages/View-Notes.tsx
+++ app/src/pages/View-Notes.tsx
@@ -1,9 +1,16 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter, IonText} from '@ionic/react';
+import { useContext, useRef } from "react";
 import './View-Notes.css';
 import '../theme/variables.css'
 import React, {useState, useEffect} from 'react';
+import SearchBar from "../components/view-note-page/SearchBar";
+import ViewList from "../components/view-note-page/viewList/ViewPastNoteList";
 
 const View_Notes: React.FC = () => {
+
+  const searchbar = useRef<HTMLDivElement>(null);
+  const viewlist = useRef<HTMLDivElement>(null);
+
   return (
     <IonPage>
       <IonHeader>
@@ -14,7 +21,8 @@ const View_Notes: React.FC = () => {
 
       <IonContent className="ion-padding">
         <div id="notes-container">
-            <IonText>This is a past note.</IonText>
+          <SearchBar ref={searchbar}/>
+          <ViewList ref={viewlist}/>
         </div>
       </IonContent>
 
@@ -27,3 +35,50 @@ const View_Notes: React.FC = () => {
 };
 
 export default View_Notes;
+
+
+
+      {/* <IonList>
+      <IonListHeader>
+        <IonLabel>Date</IonLabel>
+      </IonListHeader>
+      <IonItem>
+        <IonButton 
+          id = "viewBtn"
+          color="primary" 
+          expand="full"
+          fill="clear"
+          >
+            New Note
+      </IonButton>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Mega Man X</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>The Legend of Zelda</IonLabel>
+      </IonItem>
+      <IonItem onClick={() => console.log("hi")}>
+        <IonLabel>Pac-Man</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Super Mario World Note Here akda;dkfad;fkdjfkd;fkdjflkdsjf;dkjfakljfkajfkdjfa</IonLabel>
+      </IonItem>
+    </IonList>
+      
+        <ViewNotesModal id="login-modal" trigger="viewBtn" backdropDismiss={true}>
+                    <ViewListContainer className="center-display-flex-container">
+                        <IonText color="light">
+                            <h3>Login</h3>
+                        </IonText>
+                        <IonText color="light">
+                            <p>By continuing, you are agreeing to set up a Gratitude Notes account and agreeing to our User Agreement and Privacy Policy.</p>
+                        </IonText>
+                    </ViewListContainer>
+          </ViewNotesModal>
+      </IonContent>
+      <IonFooter>
+        <IonToolbar>
+        </IonToolbar>
+      </IonFooter>
+    </IonPage> */}
