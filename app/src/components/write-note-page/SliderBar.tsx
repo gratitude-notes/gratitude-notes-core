49b06e3ac178f6f894e4517c514f6b4f7e102d9e
al-moy729 <106407272+al-moy729@users.noreply.github.com>
2023-02-26 18:35:28 -0500

    Added the slidebar for happiness score

--- app/src/components/write-note-page/SliderBar.tsx
+++ app/src/components/write-note-page/SliderBar.tsx
@@ -3,21 +3,21 @@ import { IonRange, IonIcon } from '@ionic/react';
 import { happy, sad } from 'ionicons/icons';
 import './Sliderbar.css';
 
-const marks = [
-  { value: -5, label: '-5' },
-  { value: -4, label: '-4' },
-  { value: -3, label: '-3' },
-  { value: -2, label: '-2' },
-  { value: -1, label: '-1' },
-  { value: 0, label: '0' },
-  { value: 1, label: '1' },
-  { value: 2, label: '2' },
-  { value: 3, label: '3' },
-  { value: 4, label: '4' },
-  { value: 5, label: '5' },
-];
 
 //const tickLabels = marks.map((mark) => mark.label);
+const tickLabels = {
+  '-5': -5,
+  '-4': -4,
+  '-3': -3,
+  '-2': -2,
+  '-1': -1,
+  '0': 0,
+  '1': 1,
+  '2': 2,
+  '3': 3,
+  '4': 4,
+  '5': 5
+};
 
 const SliderBar: React.FC = () =>  {
 
@@ -26,14 +26,8 @@ const SliderBar: React.FC = () =>  {
   return (
   
       <IonRange
-        pin={true}
-        pinFormatter={(value: number) => `${value}`}
-        snaps={true} ticks={true} min={-5} max={5}
-        activeBarStart={0}
-        step={1}
         onIonChange={e => setValue(e.detail.value as number)}
-        
-        //value={value}
+        min={-5} max={5} pin={true} ticks={true} snaps={true} step={1}
     >
       <IonIcon slot="start" icon={sad}></IonIcon>
       <IonIcon slot="end" icon={happy}></IonIcon>
