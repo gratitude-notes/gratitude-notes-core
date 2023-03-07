bce17ae43ef93cf457259369d6fbc9830a76726b
Brady Waughen <brady.waughen@outlook.com>
2023-03-07 07:39:12 -0500

    Add FeedItem.tsx

--- gratitude-notes-core-skeleton/src/components/FeedList.tsx
+++ gratitude-notes-core-skeleton/src/components/FeedList.tsx
@@ -1,11 +1,14 @@
-
+import FeedItem from "./FeedItem";
 
 
 const FeedList: React.FC = () => {  
     return (
-      <div className="h-screen bg-green-300 dark:bg-slate-600">
+        <ol className="relative border-l border-gray-200 dark:border-gray-700">
+            <FeedItem />
+            <FeedItem /> 
+            <FeedItem />               
         
-      </div>
+        </ol>
     )
   }
   
--- gratitude-notes-core-skeleton/src/pages/Dashboard.tsx
+++ gratitude-notes-core-skeleton/src/pages/Dashboard.tsx
@@ -1,7 +1,9 @@
+import FeedList from "../components/FeedList";
+
 const Dashboard: React.FC = () => {  
   return (
     <div className="h-screen bg-green-300 dark:bg-slate-600">
-      
+      <FeedList />
     </div>
   )
 }
