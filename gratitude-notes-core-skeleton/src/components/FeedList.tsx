e3fc690981fc7fbe41ba4df863f227cb42907faa
Brady Waughen <brady.waughen@outlook.com>
2023-03-07 07:08:05 -0500

    Add FeedList.tsx

---
+++ gratitude-notes-core-skeleton/src/components/FeedList.tsx
@@ -0,0 +1,12 @@
+
+
+
+const FeedList: React.FC = () => {  
+    return (
+      <div className="h-screen bg-green-300 dark:bg-slate-600">
+        
+      </div>
+    )
+  }
+  
+  export default FeedList;
