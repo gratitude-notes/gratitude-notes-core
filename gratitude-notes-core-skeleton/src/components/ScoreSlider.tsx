3a866065a63590eb99aad329599e0b56ca40c013
al-moy729 <106407272+al-moy729@users.noreply.github.com>
2023-03-09 18:46:29 -0500

    Update ScoreSlider.tsx

--- gratitude-notes-core-skeleton/src/components/ScoreSlider.tsx
+++ gratitude-notes-core-skeleton/src/components/ScoreSlider.tsx
@@ -8,23 +8,42 @@ const Slider = () => {
     setValue(Number(event.target.value));
   };
 
+  const tickmarks = ['-5', '-2', '-1', '0', '1', '2', '3', '4', '5'];
+
   return (
+    <div className="items-center">
     <div className="flex flex-col items-center relative">
-      <input
-        type="range"
-        min="-5"
-        max="5"
-        value={value}
-        onChange={handleChange}
-        className="w-64 h-2 bg-gray-200 rounded-full appearance-none"
-      />
-      <div className="absolute top-0 left-0 p-2">
-        <BsFillEmojiFrownFill />
+      <div className="text-2xl text-gray-700 font-bold mb-2">{value}</div>
+      <div className='absoulute top-0'>
+        <input
+          type="range"
+          min="-5"
+          max="5"
+          value={value}
+          onChange={handleChange}
+          className="w-64 h-2 bg-pink-200 rounded-full appearance-none"
+        />
       </div>
-      <div className="absolute top-0 right-0 p-2">
+      <div className="absolute bottom-0 left-0">
         <BsFillEmojiSmileFill />
       </div>
-      <div className="mt-2 text-gray-700">{value}</div>
+      <div className="absolute bottom-0 right-0">
+        <BsFillEmojiSmileFill />
+      </div>
+      <style>
+        {`
+          input[type="range"]::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            width: 24px;
+            height: 24px;
+            background-color: #fff;
+            border: 2px solid #ccc;
+            border-radius: 50%;
+            cursor: pointer;
+          }
+        `}
+      </style>
+    </div>
     </div>
   );
 };
