97c50a14a846d8b9318a41bd8c24f7dc28ea2c38
al-moy729 <106407272+al-moy729@users.noreply.github.com>
2023-03-09 19:51:31 -0500

    Update DotMenu.tsx
    Dot menu now runs on the left

--- gratitude-notes-core-skeleton/src/components/DotMenu.tsx
+++ gratitude-notes-core-skeleton/src/components/DotMenu.tsx
@@ -32,12 +32,12 @@ const DotMenu: React.FC = () => {
 
         return (
             <div ref={ref} >
-                <button onClick={handleThemeButton} className="inline-block absolute right-6 top-2 bg-white rounded-lg hover:bg-neutral-200">
+                <button onClick={handleThemeButton} className="relative top-0 right-0 bg-white rounded-lg hover:bg-neutral-200">
                     <BsThreeDots aria-hidden="true" size={20}/>
                 </button>
 
                 {/* Dropdown Menu */}
-                <div className={`${twVisible} inline-block absolute right-0 bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}>
+                <div className={`${twVisible} absolute top right bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}>
                     <div className="py-1" role="none">
                         <button className="hover:bg-neutral-200 w-full inline-flex text-gray-700 px-4 py-2 text-sm gap-x-2">
                             Edit
