2c0cf1bdacc71daf5ba8b5863d5622d1a9ddbe71
Dinesh Umasankar <dqu5021@psu.edu>
2023-03-13 01:07:05 -0400

    Sync Branch State Attempt 1

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,40 +1,26 @@
-import { IonButton, IonContent, IonFooter, IonHeader, IonIcon, IonPage, IonText, IonTitle, IonToolbar } from "@ionic/react";
-import './Dashboard.css';
-import './global.css';
-import '../theme/variables.css';
-import logo from '../assets/GNlogo.svg';
-import { create, eye, calendar } from 'ionicons/icons';
+import FeedList from "../components/FeedList";
+import LeftSidebar from "../components/LeftSidebar";
+import RightSidebar from "../components/RightSidebar";
+import Slider from "../components/ScoreSlider";
+import SearchBar from "../components/searchBar";
+import LikeButton from "../components/LikeButton";
 
+const Dashboard: React.FC = () => {  
+  return (
+    <div className="flex flex-col bg-white dark:bg-slate-800">
+      {/* <LeftSidebar /> */}
+      <FeedList />
+      {/* <RightSidebar /> */}
 
-import { logout } from "../lib/AuthFunctions";
-
-const Dashboard: React.FC = () => {
-    return (
-        <IonPage>
-            <IonHeader className="header">
-                <IonToolbar className="header-toolbar">
-                    <IonIcon id="GNlogoIcon" icon={logo} size="large"></IonIcon>
-                    <IonButton onClick={logout}>Logout</IonButton>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent fullscreen className="content">
-                {/* <button id="add-note-btn" className="my-button">Add a Note for Today</button> */}
-            <IonButton color="tertiary" routerLink="/Write-Note"> Write Note <IonIcon slot="end" icon={create}></IonIcon> </IonButton>
-            <IonButton color="tertiary" routerLink="/View-Notes"> View Note <IonIcon slot="end" icon={eye}></IonIcon> </IonButton>
-            <IonButton color="tertiary" routerLink="/MonthCard"> Calendar <IonIcon slot="end" icon={calendar}></IonIcon> </IonButton>
-
-            </IonContent>
-            <IonFooter className="footer">
-                <div className="container">
-                    <IonTitle id="featured-note-title">Featured Note</IonTitle>
-                    <div className="secondary-container">
-                        <IonText id="featured-note-date-text">From date: 1/1/2023</IonText>
-                        <IonText id="featured-note-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</IonText>
-                    </div>
-                </div>
-            </IonFooter>
-        </IonPage>
-    );
+      <Slider />
+      <SearchBar/>
+      <div>
+        <LikeButton></LikeButton>
+      </div>
+    
+      
+    </div>
+  )
 }
 
 export default Dashboard;
