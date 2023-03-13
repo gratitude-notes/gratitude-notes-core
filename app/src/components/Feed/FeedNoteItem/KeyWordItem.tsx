b691dd951d6174b9eecdb2239e1782f837d4a5c8
Brady Waughen <brady.waughen@outlook.com>
2023-03-13 16:33:59 -0400

    Fix key in KeyWordItem

--- app/src/components/Feed/FeedNoteItem/KeyWordItem.tsx
+++ app/src/components/Feed/FeedNoteItem/KeyWordItem.tsx
@@ -7,8 +7,8 @@ const KeyWordItem: React.FC<Keyword> = ({keywords}) => {
     
     return (
         <ul className="mb-1">
-            {keywords.map((keyword: string) => (
-                <span key={keyword} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{keyword}</span>
+            {keywords.map((keyword: string, index) => (
+                <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#{keyword}</span>
             ))}
         </ul>
     )
