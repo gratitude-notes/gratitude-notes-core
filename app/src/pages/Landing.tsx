9e2dd7af65195555e42ddcd7f2fb59b861f6d1bb
Brady Waughen <brady.waughen@outlook.com>
2023-03-16 10:56:38 -0400

    Start designing Hero Section

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,15 +1,61 @@
 import Navbar from "../components/Navbar/Navbar";
 
-const Landing: React.FC = () => {  
+const Landing: React.FC = () => {
+
   return (
     <div>
-      <div className="dark:bg-slate-600 grid grid-flow-row">
+      <div className="">
         <Navbar />
-        <h2>Thank yourself.</h2>
-        <h3>Track happiness, explore joy, and live your best life today!</h3>
+
+        {/* Main Content */}
+        <div className="max-w-3xl p-8 m-auto">
+
+          {/* Hero Section */}
+          <section className="">
+            <div>
+              <h1 className="text-6xl text-center">Stay in touch with your own <span className="text-blue-500 font-bold">happiness</span>.</h1>
+            </div>
+            <div className="pt-6 pb-6">
+              <h1 className="text-xl text-center text-gray-400">Find out what makes you happy.</h1>
+            </div>
+            <div className="text-center">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
+            </div>
+          </section>
+
+          {/* Purpose and Roots */}
+          <section>
+            
+          </section>
+
+          {/* FAQ */}
+          <section>
+            
+          </section>
+
+          {/* Testimonials */}
+          <section>
+            
+          </section>
+
+          {/* Meet the team */}
+          <section>
+            
+          </section>
+
+          {/* Call to Action? */}
+          <section>
+            
+          </section>
+
+          {/* Footer */}
+          <section>
+            
+          </section>
+
+          </div>
       </div>
     </div>
-    
   )
 }
 
