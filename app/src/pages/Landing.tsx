942b4286033abd4af4eaba2d464d9a62cd8beb2d
Brady Waughen <brady.waughen@outlook.com>
2023-03-16 13:41:14 -0400

    Small features section

--- app/src/pages/Landing.tsx
+++ app/src/pages/Landing.tsx
@@ -1,4 +1,6 @@
+import { BsFillEmojiSmileFill } from "react-icons/bs";
 import Navbar from "../components/Navbar/Navbar";
+import PeopleOnPhone from "../assets/TwoPeoplePhone.png";
 
 const Landing: React.FC = () => {
 
@@ -8,52 +10,73 @@ const Landing: React.FC = () => {
         <Navbar />
 
         {/* Main Content */}
-        <div className="max-w-3xl p-8 m-auto">
+        <div className="max-w-3xl flex flex-col gap-20 p-8 m-auto">
 
           {/* Hero Section */}
-          <section className="">
+          <section className="flex flex-col gap-4">
             <div>
-              <h1 className="text-6xl text-center">Stay in touch with your own <span className="text-blue-500 font-bold">happiness</span>.</h1>
+              <h1 className="text-6xl text-center">Stay in touch with your own <span className="text-blue-500 font-bold">happiness.</span></h1>
             </div>
-            <div className="pt-6 pb-6">
+            <div>
               <h1 className="text-xl text-center text-gray-400">Find out what makes you happy.</h1>
             </div>
-            <div className="text-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
+            <div className="p-4 text-center">
+            <button className="transition ease-in-out
+                               bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300
+                               text-white font-bold py-2 px-4 rounded">
+                Get Started
+            </button>
+            </div>
+
+            {/* Phone mockup */}
+            <div className="relative flex justify-center m-auto h-[520px] w-[265px] bg-white border-8 border-black rounded-[45px]">
+              <div className="-z-50 opacity-75 absolute h-[505px] w-screen bg-[radial-gradient(circle_at_bottom,_var(--tw-gradient-stops))] from-blue-500">
+
+              </div>
+              <div className="p-4">
+                *Show example Dashboard*
+              </div>
             </div>
           </section>
 
-          {/* Purpose and Roots */}
+          {/* Call to action */}
           <section>
-            
+            <div className="flex flex-col gap-4">
+              <h1 className="text-3xl">Say what's on your mind</h1>
+              <h1 className="text-xl text-gray-400">It's time to go digital.</h1>
+              <div>
+              <button className="transition ease-in-out
+                               bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300
+                               text-white font-bold py-2 px-4 rounded">
+                Get Started
+              </button>
+              </div>
+            </div>
+          </section>
+
+          {/* Some features */}
+          <section className="flex flex-col-reverse gap-4 justify-center sm:flex-row">
+            <div className="bg-gradient-to-r from-gray-300">
+              <img src={PeopleOnPhone} alt="People on phone image"/>
+            </div>
+            <div className="flex flex-col gap-4">
+              <BsFillEmojiSmileFill className="fill-blue-500" size={25}/>
+              <h1 className="text-3xl">Explore yourself one happiness bullet at a time</h1>
+              <h1 className="text-xl text-gray-400">Communicate with your future self through your current thoughts.</h1>
+            </div>
           </section>
 
           {/* FAQ */}
-          <section>
-            
-          </section>
 
           {/* Testimonials */}
-          <section>
-            
-          </section>
 
           {/* Meet the team */}
-          <section>
-            
-          </section>
 
           {/* Call to Action? */}
-          <section>
-            
-          </section>
 
           {/* Footer */}
-          <section>
-            
-          </section>
 
-          </div>
+        </div>
       </div>
     </div>
   )
