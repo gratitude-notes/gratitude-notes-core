95f6d10abae07c14e512e3e7239098d2a52920ca
Myan <118029565+MaximTomato@users.noreply.github.com>
2023-03-20 17:17:21 -0400

    Myan-WeekInReview2
    - i fixed the weird sidescroll
    - also included what a Top Words list would look like

--- app/src/components/WeekInReview/WeekInReview.tsx
+++ app/src/components/WeekInReview/WeekInReview.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import WordCloud from 'react-d3-cloud';
 import KeywordItem from '../Feed/FeedNoteItem/KeyWordItem';
+import TopWordItem from '../WeekInReview/TopWordItem';
 
 const WeekInReview: React.FC = () => {  
 
@@ -28,9 +29,9 @@ const WeekInReview: React.FC = () => {
   ]
 
   return (
-      <div className="w-screen h-screen bg-black items-center">
+      <div className="w-screen h-screen bg-black items-center overflow-x-hidden">
         {/* MODAL */}
-        <div className="relative m-auto h-screen max-w-xl bg-white items-center">
+        <div className="relative m-auto h-screen max-w-xl bg-gray-200 items-center">
           <button className="absolute top-2 left-5 h-8 px-3 bg-black text-white rounded-full">
             Close me
           </button>
@@ -45,11 +46,10 @@ const WeekInReview: React.FC = () => {
                 <WordCloud data={data} width={200} height={200} padding={0} font={"tahoma"} spiral={"rectangular"}/>
               </div>
             </div>
-            
           </div>
 
           {/* KEYWORD HEADER */}
-          <div className="relative border-t border-r border-l border-neutral-800 bg-neutral-800 rounded-t-3xl -top-6 w-full h-full">
+          <div className="relative border-t border-r border-l border-neutral-800 bg-neutral-800 rounded-t-3xl -top-6 w-full h-1/6">
             <h1 className="text-xl text-white text-left-10 px-5 py-2">Most Frequent Topics</h1>
             {/* KEYWORD STUFF */}
             <div className="absolute px-4 py-2 border-t-4 border-r-4 border-l-4 border-neutral-600 bg-neutral-600 rounded-t-3xl w-full h-full">
@@ -57,6 +57,15 @@ const WeekInReview: React.FC = () => {
             </div>
           </div>
           
+          {/* TOP WORDS HEADER */}
+          <div className="relative border-t border-r border-l border-neutral-800 bg-neutral-800 rounded-t-3xl -top-6 w-full h-full">
+            <h1 className="text-xl text-white text-left-10 px-5 py-2">Top Words This Week</h1>
+            {/* TOP WORDS STUFF */}
+            <div className="absolute px-4 py-2 border-t-4 border-r-4 border-l-4 border-neutral-600 bg-neutral-600 rounded-t-3xl w-full h-full">
+              <TopWordItem keywords={exampleKeywords}/>
+            </div>
+          </div>
+
         </div>
       </div>
   );
