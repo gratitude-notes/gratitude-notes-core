af922d86e3393ec2f2a8bc94dacfd8099584c128
Dinesh Umasankar <dqu5021@psu.edu>
2023-03-27 23:25:16 -0400

    Favorite Notes
    - Synced with firestore
    - Favorite your notes

--- app/src/components/Feed/FeedNoteItem/LikeButton.tsx
+++ app/src/components/Feed/FeedNoteItem/LikeButton.tsx
@@ -1,22 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BsHeart, BsHeartFill } from 'react-icons/bs';
+import { setDoc, doc } from '@firebase/firestore';
+import { fb_firestore } from '../../../lib/Firebase';
+import { useSession } from '../../../lib/Session';
 
-const LikeButton: React.FC = () => {
-  const [isFilled, setIsFilled] = useState(false);
+const LikeButton: React.FC<{isFavorited: boolean, bulletDocID?: string}> = ({isFavorited, bulletDocID} : {isFavorited: boolean, bulletDocID?: string}) => {
+  const session = useSession();
+  const [isFilled, setIsFilled] = useState(isFavorited);
+
+  useEffect(() => {
+    setIsFilled(isFavorited);
+  }, [isFavorited])
+
+  const onClickLikeButton = () => {
+    if (bulletDocID && session && session.user) {
+      const likedDocRef = doc(fb_firestore, "users", session.user.uid, "notes", bulletDocID);
+      setDoc(likedDocRef, {isFavorited: !isFavorited}, {merge: true})
+    }
+  }
 
   return (
     <>
       {isFilled ? (
         <button
           className="text-white font-bold"
-          onClick={() => setIsFilled(false)}
+          onClick={onClickLikeButton}
         >
           <BsHeartFill className="inline-block fill-pink-600" />
         </button>
       ) : (
         <button
           className="text-pink-500 font-bold"
-          onClick={() => setIsFilled(true)}
+          onClick={onClickLikeButton}
         >
           {!isFilled && <BsHeart className="inline-block" />}
         </button>
