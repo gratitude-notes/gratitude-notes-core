9111106035d6b162495cc64962b04280add50fb5
Brady Waughen <brady.waughen@outlook.com>
2023-04-06 12:20:31 -0400

    [WIP] Image Dropzone

--- app/src/components/WriteNoteSequence/WriteNoteModal.tsx
+++ app/src/components/WriteNoteSequence/WriteNoteModal.tsx
@@ -20,29 +20,10 @@ const WriteNoteModal: React.FC<WriteNoteModalState> = ({updateViewState}) => {
             {/* WRITE NOTE */}
             <WriteNoteForm {...{updateViewState}}/>
 
-            {/* IMAGES */}
-            {/* <div className="px-2">
-                IMAGES INNER DIV
-                <div className="flex gap-2 overflow-x-scroll sm:justify-center sm:scrollbar-none
-                                scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-track-gray-700">
-                    <div className="w-20 aspect-square rounded-xl flex justify-center items-center border-2 border-black bg-pink-200">
-                        Image 1
-                    </div>
-                    <div className="w-20 aspect-square rounded-xl flex justify-center items-center border-2 border-black bg-pink-200">
-                        Image 2
-                    </div>
-                    <div className="w-20 aspect-square rounded-xl flex justify-center items-center border-2 border-black bg-pink-200">
-                        Image 3
-                    </div>
-                    <div className="w-20 aspect-square rounded-xl flex justify-center items-center border-2 border-black bg-pink-200">
-                        Image 4
-                    </div>
-                </div>
-            </div> */}
             {/* SCORE */}
-            <div className="flex justify-center">
+            {/* <div className="flex justify-center">
                 <input type="range" min="-5" max="5" className="w-1/2 cursor-pointer"/>
-            </div> 
+            </div>  */}
         </div>
     );
 }
