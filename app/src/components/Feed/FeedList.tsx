fa76db6a0934a4bf90821751f2ac0e652f3938f9
Brady Waughen <brady.waughen@outlook.com>
2023-04-07 15:38:36 -0400

    [WIP] Search functions

--- app/src/components/Feed/FeedList.tsx
+++ app/src/components/Feed/FeedList.tsx
@@ -1,22 +1,89 @@
+import { useState } from "react";
 import useUserBullets, { NoteBullet } from "../../hooks/useUserBullets";
 import SearchBar from "../SearchBar";
 import FeedNoteItem from "./FeedNoteItem/FeedNoteItem";
+import useComponentVisible from "../../hooks/useComponentVisible";
 
 const FeedList: React.FC = () => {
   const { bullets } = useUserBullets();
+  const [search, setSearch] = useState({
+    query: "",
+    bulletsList: bullets
+  });
+
+  const dayMap = new Map<Number, string>();
+  dayMap.set(0, "sunday");
+  dayMap.set(1, "monday");
+  dayMap.set(2, "tuesday");
+  dayMap.set(3, "wednesday");
+  dayMap.set(4, "thursday");
+  dayMap.set(5, "friday");
+  dayMap.set(6, "saturday");
+
+  const monthMap = new Map<Number, string>();
+  monthMap.set(0, "january");
+  monthMap.set(1, "february");
+  monthMap.set(2, "march");
+  monthMap.set(3, "april");
+  monthMap.set(4, "may");
+  monthMap.set(5, "june");
+  monthMap.set(6, "july");
+  monthMap.set(7, "august");
+  monthMap.set(8, "september");
+  monthMap.set(9, "october");
+  monthMap.set(10, "november");
+  monthMap.set(11, "december");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const results = bullets?.filter((bullet: NoteBullet) => {
+      if (e.target.value === "") return bullets;
+      
+      const value = e.target.value.toLowerCase();
+      const bulletTextContent = bullet.bulletTextContent.toLowerCase();
+      const bulletDay = dayMap.get(bullet.timestamp.toDate().getDay());
+      const bulletMonth = monthMap.get(bullet.timestamp.toDate().getMonth());
+      const bulletDate = bullet.timestamp.toDate().getDate().toString();
+      const bulletYear = bullet.timestamp.toDate().getFullYear().toString();
+
+      return  bulletTextContent.includes(value) ||
+              bulletDay?.includes(value) ||
+              bulletMonth?.includes(value) ||
+              bulletDate?.includes(value) ||
+              bulletYear?.includes(value);
+    })
+    setSearch(() => ({
+      query: e.target.value,
+      bulletsList: results || []
+    }));
+  }
+
+  const searchHowToVisible = useComponentVisible(false);
 
   return (
     <ol className="bg-white dark:bg-gray-800
                   scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-track-gray-700">
       {/* SEARCH FOR MOBILE */}
-      <div className="xl:hidden">
-        <SearchBar />
+      <div className="px-2 xl:hidden">
+        <SearchBar handleSearchChange={handleSearchChange}/>
       </div>
 
-      {/* BULLETS LIST */}
-      {bullets?.map((bullet: NoteBullet, index: number) => (
-        <FeedNoteItem key={index} {...bullet} />
-      ))}
+      {(search.query === "")
+        ?
+          <>
+            {/* NO QUERY -> show all bullets */}
+            {bullets?.map((bullet: NoteBullet, index: number) => (
+              <FeedNoteItem key={index} {...bullet} />
+            ))}
+          </>
+        :
+          <>
+            {/* QUERIED LIST */}
+            {search.bulletsList?.map((bullet: NoteBullet, index: number) => (
+              <FeedNoteItem key={index} {...bullet} />
+            ))}
+          </>
+      }
+
     </ol>
   )
 }
--- app/src/components/SearchBar.tsx
+++ app/src/components/SearchBar.tsx
@@ -1,74 +1,18 @@
-import { useState } from "react";
-import { BsChevronDown, BsSearch } from "react-icons/bs";
-import useComponentVisible from "../hooks/useComponentVisible";
 
-const Searchbar: React.FC = () => {  
+type SearchbarProps = {
+    handleSearchChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-    const [currentSearchCatergory, setCurrentSearchCatergory] = useState("Search by");
-    const [search, setSearch] = useState('');
-
-    const updateCurrentSearch = (current: string) => {
-        if ((current === "Favorites") || (current === "Keywords") || (current === "Scores")) setCurrentSearchCatergory(current);
-        else setCurrentSearchCatergory("Search by");
-    }
-
-    const searchDropdownVisible = useComponentVisible(false);
-
-    const handleSearchDropdown = () => {
-        (searchDropdownVisible.isComponentVisible) ? searchDropdownVisible.setComponentVisible(false) : searchDropdownVisible.setComponentVisible(true);
-    }
-
-    const searchDropdownState = (searchDropdownVisible.isComponentVisible) ? "visible" : "hidden";
-    
-    const handleSearchButton = () => {
-        setCurrentSearchCatergory("Search by");
-    }
-
-    const handleKeyDown = (event: any) => {
-        if (event.key === 'Enter') {
-          console.log("Enter key pressed");
-          console.log("Search", search);
-        }
-      };
-
-    const searchInputState = (currentSearchCatergory === "Search by") ? true : false;
+const Searchbar: React.FC<SearchbarProps> = ({handleSearchChange}) => {  
 
     return (
-        <div ref={searchDropdownVisible.ref} className="relative px-2 xl:px-0">
-            <form className="p-1 gap-2 flex flex-wrap border border-gray-400 dark:border-gray-600 rounded-lg xl:border-0 items-center text-md font-semibold text-black dark:text-white">
-                <div onClick={handleSearchDropdown} className="gap-2 flex hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md cursor-pointer items-center">
-                    <div>{currentSearchCatergory}</div>
-                    <BsChevronDown />
-                </div>
-
-                <input className="flex-grow px-1 border border-gray-400 dark:border-gray-600 text-black rounded-lg" type="search" name="search"
-                        placeholder={`Search${(currentSearchCatergory === "Search by") ? "" : " " + currentSearchCatergory}...`}
-                        disabled={(searchInputState === true) ? true : false}
-                        onChange={event => setSearch(event.target.value)}
-                        onKeyDown={handleKeyDown}/>
-            </form>
-
-            {/* Dropdown Menu */}
-            <div className={`${searchDropdownState} absolute top-full z-50`}>
-                <div className="flex flex-col gap-1 bg-white dark:bg-gray-800 rounded-md text-black dark:text-white shadow-lg shadow-gray-900">
-                    <div onClick={() => {updateCurrentSearch("Favorites"); handleSearchDropdown();}}
-                        className="cursor-pointer p-2 text-sm
-                                    hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md transition-colors duration-100 ease-in">
-                        Favorites
-                    </div>
-                    <div onClick={() => {updateCurrentSearch("Keywords"); handleSearchDropdown();}}
-                        className="cursor-pointer p-2 text-sm
-                                    hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md transition-colors duration-100 ease-in">
-                        Keywords
-                    </div>
-                    <div onClick={() => {updateCurrentSearch("Scores"); handleSearchDropdown();}}
-                        className="cursor-pointer p-2 text-sm
-                                    hover:bg-gray-300 dark:hover:bg-gray-600 rounded-md transition-colors duration-100 ease-in">
-                        Scores
-                    </div>      
-                </div>
-            </div>
-        </div>
+        <input className="w-full px-2 py-1
+                        bg-gray-200 dark:bg-gray-900 
+                        focus:bg-white focus:dark:bg-gray-800
+                        focus:border-none focus:outline-2 focus:outline-cyan-500
+                        text-black dark:text-white 
+                        rounded-lg"
+                        type="search"placeholder="Search" onChange={handleSearchChange}/>
     );
 }
 
