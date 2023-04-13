877e8daa237953b12f499042a332aadb939e8c16
Dinesh Umasankar <dqu5021@psu.edu>
2023-04-13 00:12:57 -0400

    Fix Settings Modal Toggle Switch

--- app/src/components/SettingsModal/SettingsModal.tsx
+++ app/src/components/SettingsModal/SettingsModal.tsx
@@ -36,7 +36,7 @@ const SettingsModal: React.FC<SettingsModalState> = ({updateViewState}) => {
     }
 
     return (
-        <div className={`py-2 px-4 md:px-[100px] lg:px-[200px] flex flex-col gap-6 flex-grow overflow-y-auto`}>
+        <div className={`py-2 px-4 md:px-[100px] lg:px-[200px] flex flex-col gap-6 flex-grow overflow-y-auto select-none`}>
             {/* HEADER AND PROFILE */}
             <div className="flex justify-between text-black dark:text-white">
                 <button onClick={() => updateViewState("Home")}><BsArrowLeft size={20}/></button>
@@ -50,7 +50,7 @@ const SettingsModal: React.FC<SettingsModalState> = ({updateViewState}) => {
                             scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 dark:scrollbar-track-gray-700">
                 <div className="justify-center text-center py-1">
                     <label className="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" value="" checked={isLocationSharing} onChange={updateUserSettings} className="peer"/>
+                    <input type="checkbox" value="" checked={isLocationSharing} onChange={updateUserSettings} className="peer hidden"/>
                     <div className="w-11 h-6 bg-cyan-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-cyan-300 dark:peer-focus:ring-cyan-800 rounded-full peer dark:bg-cyan-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-cyan-500"></div>
                     <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Location Sharing</span>
                     </label>
