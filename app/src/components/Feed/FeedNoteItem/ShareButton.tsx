0bfd68c4bba231afb8c01a3f31f95a04a469c9c3
Dinesh Umasankar <dqu5021@psu.edu>
2023-04-16 00:54:34 -0400

    Render Share/Copy Button Based On Device Type

--- app/src/components/Feed/FeedNoteItem/ShareButton.tsx
+++ app/src/components/Feed/FeedNoteItem/ShareButton.tsx
@@ -1,60 +1,73 @@
-import { BsShare } from 'react-icons/bs';
+import { BsClipboard, BsShare } from 'react-icons/bs';
 import { Share } from '@capacitor/share';
 import { Device } from '@capacitor/device';
 import { Clipboard } from '@capacitor/clipboard';
 import toast from 'react-hot-toast';
 import { Timestamp } from '@firebase/firestore';
+import { useEffect, useState } from 'react';
 
 type ShareButtonProps = {
-  shareText: string
-  shareDate: Timestamp,
-  shareAddress: string | null
+    shareText: string
+    shareDate: Timestamp,
+    shareAddress: string | null
 }
 
 const ShareButton: React.FC<ShareButtonProps> = ({ shareText, shareDate, shareAddress }) => {
-
-  const onClickShareButton = async () => {
-
-    const deviceInfo = await Device.getInfo();
-    const shareDateStr = shareDate.toDate().toLocaleString(
-        'en-US',
-        { month: '2-digit', day: '2-digit', year: 'numeric' }
-    );
+    const [shareButtonIcon, setShareButtonIcon] = useState<JSX.Element>(<BsClipboard size={24} />);
     
-    const shareTimeStr = shareDate.toDate().toLocaleString(
-        'en-US',
-        { hour: 'numeric', minute: '2-digit', hour12: true }
-    );
+    const onClickShareButton = async () => {
+        const deviceInfo = await Device.getInfo();
+        
+        const shareDateStr = shareDate.toDate().toLocaleString(
+            'en-US',
+            { month: '2-digit', day: '2-digit', year: 'numeric' }
+        );
 
-    const shareFormattedTime = `${shareDateStr} • ${shareTimeStr}`;
-    const shareFormattedText = shareText.trim();
-    const shareFormattedAddress = (shareAddress) ? `${shareAddress.trim()}\n` : '';
-    const shareString = `${shareFormattedText}\n\n${shareFormattedTime}\n${shareFormattedAddress}\nShared via DoseHappiness - https://dosehappiness.com/`;
+        const shareTimeStr = shareDate.toDate().toLocaleString(
+            'en-US',
+            { hour: 'numeric', minute: '2-digit', hour12: true }
+        );
 
-    if (deviceInfo.platform === 'web') {
-        await Clipboard.write({
-            string: shareString
-        });
-        toast.success('Copied to clipboard!');
-    } else {
-        await Share.share({
-            title: 'Share your note with someone!',
-            text: shareText,
-            url: 'https://dosehappiness.com/',
-            dialogTitle: 'Share your note with someone!'
-        });
+        const shareFormattedTime = `${shareDateStr} • ${shareTimeStr}`;
+        const shareFormattedText = shareText.trim();
+        const shareFormattedAddress = (shareAddress) ? `${shareAddress.trim()}\n` : '';
+        const shareString = `${shareFormattedText}\n\n${shareFormattedTime}\n${shareFormattedAddress}\nShared via DoseHappiness - https://dosehappiness.com/`;
+
+        if (deviceInfo.platform === 'web') {
+            await Clipboard.write({
+                string: shareString
+            });
+            toast.success('Copied to clipboard!');
+        } else {
+            await Share.share({
+                title: 'Share your note with someone!',
+                text: shareText,
+                url: 'https://dosehappiness.com/',
+                dialogTitle: 'Share your note with someone!'
+            });
+        }
     }
 
+    useEffect(() => {
+        const getShareButtonIcon = async () => {
+            const deviceInfo = await Device.getInfo();
+            if (deviceInfo.platform === 'web') {
+                setShareButtonIcon(<BsClipboard size={15} />);
+            } else {
+                setShareButtonIcon(<BsShare size={15} />);
+            }
+        }
 
-  }
+        getShareButtonIcon();
+    }, []);
 
-  return (
-    <>
-      <button onClick={onClickShareButton} className="hover:bg-green-700 hover:bg-opacity-20 hover:text-green-500 p-2 hover:rounded-full">
-        <BsShare size={15} />
-      </button>
-    </>
-  );
+    return (
+        <>
+            <button onClick={onClickShareButton} className="hover:bg-green-700 hover:bg-opacity-20 hover:text-green-500 p-2 hover:rounded-full">
+                {shareButtonIcon}
+            </button>
+        </>
+    );
 }
 
 export default ShareButton;
